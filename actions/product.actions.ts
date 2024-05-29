d3f898799780b606b66027e9e4f95f6c5300a9f6
nomanmajeed <nomanmajeed986@gmail.com>
2024-05-30 02:27:23 +0500

    fix: product create server action

--- actions/product.actions.ts
+++ actions/product.actions.ts
@@ -13,19 +13,70 @@ export const getProducts = async () => {
 
 // action to get a single product
 export const getProduct = async (id: number) => {
-  return await prisma.product.findUnique({
-    where: {
-      id: id,
-    },
-  });
-};
+    return await prisma.product.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        topPanelFillings: true,
+        bottomPanelFillings: true,
+        borderPanelFillings: true,
+      },
+    });
+  };
 
 // action to create a product
 export const createProduct = async (data: IFormStore) => {
-  return await prisma.product.create({
-    data: data,
-  });
-};
+    return await prisma.product.create({
+      data: {
+        productName: data.productName,
+        productDetails: data.productDetails,
+        deliveryAddress: data.deliveryAddress,
+        tickSupplier: data.tickSupplier,
+        tickQuality: data.tickQuality,
+        tickNumberRef: data.tickNumberRef,
+        tickColourRef: data.tickColourRef,
+        composition: data.composition,
+        issuedTo: data.issuedTo,
+        dateRequired: data.dateRequired,
+        comments: data.comments,
+        labelType: data.labelType,
+        springType: data.springType,
+        quiltType: data.quiltType,
+        accessories: data.accessories,
+        patternNumber: data.patternNumber,
+        borderType: data.borderType,
+        borderDepth: data.borderDepth,
+        topPanelFillings: {
+          create: data.panelFillingTopLayer.map((filling) => ({
+            layer: filling.layer,
+            description: filling.description,
+            weight: filling.weight,
+            sizeWidth: filling.sizeWidth,
+            supplier: filling.supplier,
+          })),
+        },
+        bottomPanelFillings: {
+          create: data.panelFillingBottomLayer.map((filling) => ({
+            layer: filling.layer,
+            description: filling.description,
+            weight: filling.weight,
+            sizeWidth: filling.sizeWidth,
+            supplier: filling.supplier,
+          })),
+        },
+        borderPanelFillings: {
+          create: data.borderFilling.map((filling) => ({
+            layer: filling.layer,
+            description: filling.description,
+            weight: filling.weight,
+            sizeWidth: filling.sizeWidth,
+            supplier: filling.supplier,
+          })),
+        },
+      },
+    });
+  };
 
 // action to delete a product
 export const deleteProduct = async (id: number) => {
