e26851e3cf71452d37fd60670d6892c95c263665
nomanmajeed <nomanmajeed986@gmail.com>
2024-05-31 23:55:40 +0500

    feat: update product server action

--- actions/product.actions.ts
+++ actions/product.actions.ts
@@ -78,6 +78,65 @@ export const createProduct = async (data: IFormStore) => {
     });
   };
 
+
+// action to update a product
+export const updateProduct = async (id: number, data: IFormStore) => {
+    return await prisma.product.update({
+      where: { id: id },
+      data: {
+        productName: data.productName,
+        productDetails: data.productDetails,
+        deliveryAddress: data.deliveryAddress,
+        tickSupplier: data.tickSupplier,
+        tickQuality: data.tickQuality,
+        tickNumberRef: data.tickNumberRef,
+        tickColourRef: data.tickColourRef,
+        composition: data.composition,
+        issuedTo: data.issuedTo,
+        dateRequired: data.dateRequired,
+        comments: data.comments,
+        labelType: data.labelType,
+        springType: data.springType,
+        quiltType: data.quiltType,
+        accessories: data.accessories,
+        patternNumber: data.patternNumber,
+        borderType: data.borderType,
+        borderDepth: data.borderDepth,
+        topPanelFillings: {
+          deleteMany: {}, // Delete all existing top panel fillings
+          create: data.panelFillingTopLayer.map((filling) => ({
+            layer: filling.layer,
+            description: filling.description,
+            weight: filling.weight,
+            sizeWidth: filling.sizeWidth,
+            supplier: filling.supplier,
+          })),
+        },
+        bottomPanelFillings: {
+          deleteMany: {}, // Delete all existing bottom panel fillings
+          create: data.panelFillingBottomLayer.map((filling) => ({
+            layer: filling.layer,
+            description: filling.description,
+            weight: filling.weight,
+            sizeWidth: filling.sizeWidth,
+            supplier: filling.supplier,
+          })),
+        },
+        borderPanelFillings: {
+          deleteMany: {}, // Delete all existing border panel fillings
+          create: data.borderFilling.map((filling) => ({
+            layer: filling.layer,
+            description: filling.description,
+            weight: filling.weight,
+            sizeWidth: filling.sizeWidth,
+            supplier: filling.supplier,
+          })),
+        },
+      },
+    });
+  };
+  
+
 // action to delete a product
 export const deleteProduct = async (id: number) => {
   return await prisma.product.delete({
