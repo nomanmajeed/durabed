2ff2d4582ba7639815257626f5a1eb713cfe3a2f
nomanmajeed <nomanmajeed986@gmail.com>
2024-06-01 02:06:37 +0500

    fix: missing dependencies and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 export default nextConfig;
