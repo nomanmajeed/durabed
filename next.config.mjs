4d5a4d1867a0f509818f791354fec9e352fb26b0
nomanmajeed <nomanmajeed986@gmail.com>
2024-06-02 01:35:35 +0500

    fix: build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '*',
+      },
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve('.');
+    return config;
+  },
+};
 
 export default nextConfig;
